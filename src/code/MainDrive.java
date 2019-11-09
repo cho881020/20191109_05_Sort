ccd22a7415d2787962ecab55cf2554f33d8e97af
조경진 <kj_cho@idealidea.co.kr>
2019-11-09 14:41:48 +0900

    문제 배열 생성 / 출력
     - 순서가 제멋대로인것을 정렬하기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,6 +4,16 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
+//		순서가 뒤죽박죽인 배열.
+		int[] numbers = {8,1,7,2,6,12,11};
+		
+//		어떻게 해야 1~12의 순서로 출력?
+		
+//		배열 안의 숫자들을 출력.
+		for (int num : numbers) {
+			System.out.println(num);
+		}
+		
 	}
 	
 }
